7ec21a02a6943bcc61398f63947065d493699754
ruidoc <2218466341@qq.com>
2023-06-27 07:40:49 +0800

    feat: 初始化提交

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import ElementPlus from "element-plus";
+
+import App from "./App.vue";
+import router from "./router";
+
+import "./styles/main.less";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(ElementPlus);
+
+app.mount("#app");
